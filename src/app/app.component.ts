14d5078f896ccf83adfe6fd9a5fed9d106b4aa54
rolitodev <hectorj.24@hotmail.com>
2024-04-05 14:20:38 -0500

    Add new version with SSR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject, HostBinding } from '@angular/core';
+import { initFlowbite } from "flowbite";
+import { isPlatformBrowser } from "@angular/common";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +10,34 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'jogastars_frontend';
+
+  @HostBinding('class.dark') get mode() {
+    return this.isDarkMode;
+  }
+
+  public isDarkMode: boolean = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      initFlowbite();
+    }
+    if (typeof window !== "undefined") {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        console.log('USER IN DARK MODE');
+        this.isDarkMode = true;
+      } else {
+        console.log('USER IN LIGHT MODE');
+        this.isDarkMode = false;
+      }
+   }
+  }
+
+  changeThemeMode(): void {
+    this.isDarkMode = !this.isDarkMode;
+  }
+
 }
