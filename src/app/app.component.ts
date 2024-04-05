6aa5d9187a83a20f91224b82bc72af7558f55e41
rolitodev <hectorj.24@hotmail.com>
2024-04-05 13:29:19 -0500

    ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,13 @@
-import { Component, HostBinding, effect, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-
-  public darkMode = signal<boolean>(JSON.parse(window.localStorage.getItem('darkMode') ?? 'false'));
-
-  @HostBinding('class.dark') get mode() {
-    return this.darkMode();
-  }
-
-  constructor() {
-
-    effect(() => {
-      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
-    });
-
-    let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
-    let isDarkMode = mediaQueryObj.matches;
-    // Ya está lista la función que detecta el tema del navegador: claro | oscuro
-    // pero hay que continuar desarrollandola
-    console.log('isDarkMode', isDarkMode);
-
-  }
-
-  ngOnInit(): void {
-    // Se inicializa la librería de los componentes UI.
-    initFlowbite();
-  }
-
+  title = 'jogastars_frontend';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('../app/pages/login/login.component').then(m => m.LoginComponent)
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('../app/pages/register/register.component').then(m => m.RegisterComponent)
-    },
-    {
-        path: '**',
-        loadComponent: () => import('../app/pages/not-found/not-found.component').then(m => m.NotFoundComponent)
-    }
-];
+export const routes: Routes = [];
